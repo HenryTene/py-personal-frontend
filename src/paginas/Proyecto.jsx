180461b1c7bfb319031b988e49805ce896c168d1
Henry Tene <henry_tene@hotmail.com>
2022-05-21 01:24:10 -0500

    Se agrega un spinner antes de ver la informacion del proyecto

--- src/paginas/Proyecto.jsx
+++ src/paginas/Proyecto.jsx
@@ -2,16 +2,25 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
 
-
 const Proyecto = () => {
-    const params = useParams();
-    const {obtenerProyecto} = useProyectos();
+  const params = useParams();
+  const { obtenerProyecto, proyecto, cargando } = useProyectos();
 
-    useEffect(() => {
-        obtenerProyecto(params.id);
-    } , []);
+  useEffect(() => {
+    obtenerProyecto(params.id);
+  }, []);
 
-  return <div>Proyecto</div>;
+  const { nombre } = proyecto;
+
+  return cargando ? (
+    <div class=" flex justify-center items-center">
+      <div class="animate-spin rounded-full h-8 w-8 border-b-2 border-sky-900"></div>
+    </div>
+  ) : (
+    <div>
+      <h1 className="font-black text-4xl">{nombre}</h1>
+    </div>
+  );
 };
 
 export default Proyecto;
