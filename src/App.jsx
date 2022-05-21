708123f603ef7b30d83119e815feb8a61ffebc6c
Henry Tene <henry_tene@hotmail.com>
2022-05-21 02:03:22 -0500

    Se añade un routing para editar el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import NuevoProyecto from "./paginas/NuevoProyecto";
 import { AuthProvider } from "./context/AuthProvider";
 import { ProyectosProvider } from "./context/ProyectosProvider";
 import Proyecto from "./paginas/Proyecto";
+import EditarProyecto from "./paginas/EditarProyecto";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
               <Route index element={<Proyectos />} />
               <Route path="crear-proyecto" element={<NuevoProyecto />} />
               <Route path=":id" element={<Proyecto />} />
+              <Route path="editar/:id" element={<EditarProyecto />} />
             </Route>
           </Routes>
         </ProyectosProvider>
