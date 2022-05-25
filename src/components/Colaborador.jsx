75e4d7e5526d8a617c170b880e29f6fec72230d8
Henry Tene <henry_tene@hotmail.com>
2022-05-24 21:51:29 -0500

    Se diseña el modal para detectar y eliminar al colaborador

--- src/components/Colaborador.jsx
+++ src/components/Colaborador.jsx
@@ -1,4 +1,8 @@
+import useProyectos from "../hooks/useProyectos";
+
 const Colaborador = ({ colaborador }) => {
+
+    const {handleModalEliminarColaborador} = useProyectos();
   const { nombre, email } = colaborador;
   return (
     <div className="border-b p-5 flex justify-between items-center">
@@ -9,7 +13,9 @@ const Colaborador = ({ colaborador }) => {
       <div>
             <button 
             type="button"
-            className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg">
+            className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
+            onClick={() => handleModalEliminarColaborador(colaborador)}
+            >
                 Eliminar
             </button>
       </div>
--- src/paginas/Proyecto.jsx
+++ src/paginas/Proyecto.jsx
@@ -3,6 +3,7 @@ import { useParams, Link } from "react-router-dom";
 import useProyectos from "../hooks/useProyectos";
 import ModalFormularioTarea from "../components/ModalFormularioTarea";
 import ModalEliminarTarea from "../components/ModalEliminarTarea";
+import ModalEliminarColaborador from "../components/ModalEliminarColaborador";
 import Tarea from "../components/Tarea";
 import Alerta from "../components/Alerta";
 import Colaborador from "../components/Colaborador";
@@ -18,7 +19,7 @@ const Proyecto = () => {
 
   const { nombre } = proyecto;
 
-  console.log(proyecto);
+  
 
   if (cargando) return "Cargando...";
   const { msg } = alerta;
@@ -107,7 +108,7 @@ const Proyecto = () => {
             <Colaborador
             key={ colaborador._id }
             colaborador={ colaborador }
-            
+
             />
           ))
         ) : (
@@ -119,6 +120,7 @@ const Proyecto = () => {
 
       <ModalFormularioTarea />
       <ModalEliminarTarea />
+      <ModalEliminarColaborador />
     </>
   );
 };
